7df2610c1c3d3bc34cd3101271c953513d5c18f3
Cordoba <agustincordoba754@gmail.com>
2025-06-03 22:56:44 -0300

    Update #01 variables.py

--- #01 variables.py
+++ #01 variables.py
@@ -1,7 +1,3 @@
-
-
-### Variables ###
-
 mi_variable= "variable 1"
 print(mi_variable)
 
@@ -15,31 +11,30 @@ print(type(mi_variable_int_a_str))
 mi_variable_boolanea = False
 print(mi_variable_boolanea)
 
-# Concatenación de variables en un print
+
 print(mi_variable, mi_variable_int_a_str, mi_variable_boolanea)
 print("Este es el valor de:", mi_variable_boolanea)
 
-# Algunas funciones del sistema
+
 print(len(mi_variable))
 
-# Variables en una sola línea. ¡Cuidado con abusar de esta sintaxis!
+
 nombre, apellido, alias, edad = "Hernán", "Córdoba", 'No tengo apodo', 17
 print("Me llamo:", nombre, apellido, ". Mi edad es:",
       edad, ". Y mi apodo es:", alias)
 
-# Inputs
+
 nombre1 = input('¿Cuál es tu nombre? ')
 edad1 = input('¿Cuántos años tienes? ')
 print(nombre1)
 print(edad1)
 
-# Cambiamos su tipo
+
 nombre = 35
 edad = "Hernan"
 print(nombre)
 print(edad)
 
-# ¿Forzamos el tipo?
 address: str = "Mi dirección"
 address = True
 address = 7
