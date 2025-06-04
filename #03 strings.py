98c951e187961bb019a94748264e63b5aabfc3a0
Cordoba <agustincordoba754@gmail.com>
2025-06-03 22:57:23 -0300

    Update #03 strings.py

--- #03 strings.py
+++ #03 strings.py
@@ -1,5 +1,3 @@
-### Strings ###
-
 mi_string = "Mi String"
 mi_otro_string = 'Mi otro String'
 
@@ -16,7 +14,7 @@ print(my_tab_string)
 my_scape_string = "\\tEste es un String \\n escapado"
 print(my_scape_string)
 
-# Formateo
+
 
 name, surname, age = "Brais", "Moure", 35
 print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age))
@@ -24,14 +22,14 @@ print("Mi nombre es %s %s y mi edad es %d" % (name, surname, age))
 print("Mi nombre es " + name + " " + surname + " y mi edad es " + str(age))
 print(f"Mi nombre es {name} {surname} y mi edad es {age}")
 
-# Desempaqueado de caracteres
+
 
 language = "python"
 a, b, c, d, e, f = language
 print(a)
 print(e)
 
-# División
+
 
 language_slice = language[1:3]
 print(language_slice)
@@ -45,12 +43,12 @@ print(language_slice)
 language_slice = language[0:6:2]
 print(language_slice)
 
-# Reverse
+
 
 reversed_language = language[::-1]
 print(reversed_language)
 
-# Funciones del lenguaje
+
 
 print(language.capitalize())
 print(language.upper())
@@ -60,4 +58,4 @@ print("1".isnumeric())
 print(language.lower())
 print(language.lower().isupper())
 print(language.startswith("Py"))
-print("Py" == "py")  # No es lo mismo
+print("Py" == "py")  
