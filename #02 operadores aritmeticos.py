3e0a1c6824b4ed7397ec25bd58c7bde74a1e2cd6
Cordoba <agustincordoba754@gmail.com>
2025-06-03 22:57:04 -0300

    Update #02 operadores aritmeticos.py

--- #02 operadores aritmeticos.py
+++ #02 operadores aritmeticos.py
@@ -1,48 +1,44 @@
+print(2 + 4) 
+print(5 - 4) 
+print(8 * 4) 
+print(24 / 4) 
+print(10 % 3) 
+print(10 // 3) 
+print(2 ** 3) 
+print(2 ** 3 + 3 - 7 / 1 // 4)
 
-### Operadores Aritméticos ###
 
-# Operaciones con enteros
-print(2 + 4) #suma
-print(5 - 4) #resta 
-print(8 * 4) #multiplicacion
-print(24 / 4) #division
-print(10 % 3) #da el resto de la division
-print(10 // 3) #da el cociente
-print(2 ** 3) #potencia
-print(2 ** 3 + 3 - 7 / 1 // 4)#combinacion
-
-# Operaciones con cadenas de texto
 print("Hola " + "mundo " + "como va?")
 print("Hola " + str(5))
 
-# Operaciones mixtas
+
 print("andate riquelme " * 5)
 print("andate riquelme " * (2 ** 3))
 
 my_float = 2.5 * 2
 print("Hola " * int(my_float))
 
-### Operadores Comparativos ###
-
-# Operaciones con enteros
-print(3 > 4) #mayor que 
-print(3 < 4) #menor que 
-print(3 >= 4) #mayor o igual que
-print(4 <= 4) #menos o igual que
-print(3 == 4) #igual que
-print(3 != 4) #ya no se que es eso 
 
 
-# Operaciones con cadenas de texto
+
+print(3 > 4) 
+print(3 < 4)  
+print(3 >= 4)
+print(4 <= 4)
+print(3 == 4)
+print(3 != 4) 
+
+
+
 print("Hola" > "chauu")
 print("Hola" < "chauu")
-print("aaaaaaa" >= "aaaaab")  # Ordenación alfabética por ASCII
-print(len("aaaaa") >= len("abaa"))  # Cuenta caracteres
+print("aaaaaaa" >= "aaaaab")  
+print(len("aaaaa") >= len("abaa"))
 print("Hola" <= "Python")
 print("Hola" == "Hola")
 print("Hola" != "Python")
 
-### Operadores Lógicos ###
+
 
 print(3 > 4 and "Hola" > "Python")
 print(3 > 4 or "Hola" > "Python")
