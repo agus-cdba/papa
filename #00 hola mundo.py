7f85b7b718b18ca16c7c6647d07822e7e6dd083c
Cordoba <agustincordoba754@gmail.com>
2025-06-03 22:56:22 -0300

    Update #00 hola mundo.py

--- #00 hola mundo.py
+++ #00 hola mundo.py
@@ -1,9 +1,9 @@
-print("Hola Python")
-print('Hola Python')
+print("Hola addamo")
+print('Hola addamo')
 
-print(type("hola soy str"))  # Tipo 'str'
-print(type(9))  # Tipo 'int'
-print(type(3.2))  # Tipo 'float'
-print(type(4 + 9j))  # Tipo 'complex'
-print(type(True))  # Tipo 'bool'
-print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
+print(type("hola soy messi"))  
+print(type(9)) 
+print(type(3.2))
+print(type(4 + 9j))  
+print(type(True)) 
+print(type(print("hola que haces")))
